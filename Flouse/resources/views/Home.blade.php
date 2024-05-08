9ac9e0ce318e622a55e72349c0cdfc9947ad5e71
LAPTOP-TTUIQ39L\ichsan <ichsanilyasa3@gmail.com>
2024-05-08 15:38:12 +0700

    First App update : 1.0.0 - Homepage - navbar

---
+++ Flouse/resources/views/Home.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../CSS/Public.css">
+        <link rel="stylesheet" href="../CSS/Home.css">
+        <title>Flouse</title>
+    </head>
+
+    <body>
+        <div class="container-home">
+            @include('Navbar')
+            <div class="landing-page-01">
+                <div class="landing-page-01-left">
+                    <img src="../image/LandingPage01.svg" alt="" class="landingpage01-mod">
+                </div>
+                <div class="landing-page-01-right">
+                    <img src="../image/FlowerHomePage.svg" alt="" class="image-flower-home-mod">
+                    <div class="page-01-title-sub-button">
+                        <div class="page-01-title">
+                            Hydrangea macrophylla
+                        </div>
+                        <div class="page-01-sub-title">
+                            Hydrangeas area genus of woody, deciduous, perennial shrubs in the Hydrangeaceae family, with 48 types.They are native to
+                            Japan, China, Korea, and Southeast Asia.
+                        </div>
+                        <div class="discover-more-button">
+                            <img src="../image/discovermore.svg" alt="" class="discover-more-button-mod">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="landing-page-02">
+                <div class="landing-page-02-banner-01">
+                    <img src="../image/sample1.svg" alt="" width="885" height="379">
+                    <div class="landing-page-02-part-01">
+                        <div class="landing-page-02-title">House Plant / Verdent Villa</div>
+                        <div class="landing-page-02-sub-title">Step into Verdent Villa, where lush houseplants transform every room into a green
+                            oasis. From cascading vines to aromatic herbs, our sanctuary brings nature indoors for a tranquil living experience.</div>
+                        <div class="landing-page-02-explore-button-right"><button class="button-mod-for-landing">Explore our latest</button></div>
+                    </div>
+                </div>
+                <div class="landing-page-02-banner-02">
+                    <img src="../image/sample2.svg" alt="" width="885" height="379">
+                    <div class="landing-page-02-part-02">
+                        <div class="landing-page-02-title">Plants Care /
+                            Flora Concierge</div>
+                        <div class="landing-page-02-sub-title">Introducing Plants Care: Your Botanical Butler Service. Let us tend to your plants,
+                            ensuring they thrive and flourish in your home or office</div>
+                        <div class="landing-page-02-explore-button"><button class="button-mod-for-landing">Explore our latest</button></div>
+                    </div>
+                </div>
+            </div>
+            <div class="landing-page-03">
+                <div class="landing-page-02-title"></div>
+                <div class="landing-page-02-sub-title"></div>
+                <div class="landing-page-02-explore-button"></div>
+            </div>
+            <div class="landing-page-04">
+
+            </div>
+        </div>
+        <footer></footer>
+    </body>
+
+</html>
--- Flouse/routes/web.php
+++ Flouse/routes/web.php
@@ -2,4 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { return view('landing'); });
+Route::get('/', function () {
+    return view('Home');
+});
+
+
+Route::get('/navbar', function () {
+    return view('Navbar');
+});
