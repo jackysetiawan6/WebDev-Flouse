36b19c6dbcc692bb5719c14781f66f32a337481e
Jacky Setiawan <jackysetiawan6@gmail.com>
2024-05-13 15:11:50 +0700

    Finished store page.

--- Flouse/routes/web.php
+++ Flouse/routes/web.php
@@ -6,7 +6,6 @@ Route::get('/', function () {
     return view('Home');
 });
 
-
 Route::get('/navbar', function () {
     return view('Navbar');
 });
@@ -18,3 +17,26 @@ Route::get('/footer', function () {
 Route::get('/login', function () {
     return view('Login');
 });
+
+Route::get('/store', function () {
+    $categories = [
+        'Aster',
+        'Calla Lily',
+        'Carnations',
+        'Chrysanthemum',
+        'Daisy',
+        'Snapdragon'
+    ];
+    $colors = [
+        'Red',
+        'Orange',
+        'Yellow',
+        'Green',
+        'Blue',
+        'Purple',
+        'Pink',
+        'White',
+        'Black'
+    ];
+    return view('Store', ['categories' => $categories, 'colors' => $colors]);
+})->name('store');
