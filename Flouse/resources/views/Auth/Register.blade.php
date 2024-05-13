2bc96e95d6f8abcfd159f7b6088c307f8bb4915c
Stephen William <stephen.william001@binus.ac.id>
2024-05-13 21:42:38 +0700

    Adding Password Toggle to Register Page

--- Flouse/resources/views/Auth/Register.blade.php
+++ Flouse/resources/views/Auth/Register.blade.php
@@ -24,10 +24,15 @@
                     <label class="input-label email">Email</label>
                     <p class="error-msg">Invalid email (example: yourname@email.com)</p>
                 </div>
-                <div class="input-box pass">
-                    <input required="" type="password" class="input-text pass" id="pass-text">
-                    <label class="input-label pass">Password</label>
-                    <p class="error-msg">Must consist of minumum 8 alphanum</p>
+                <div class="input-box outer">
+                    <div class="input-box pass">
+                        <input required="" type="password" class="input-text pass" id="pass-text">
+                        <svg class="show-hide-pass" id="pass-toggle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#262b36" d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
+                        <label class="input-label pass">Password</label>
+                    </div>
+                    <div>
+                        <p class="error-msg">Must consist of minumum 8 alphanum</p>
+                    </div>
                 </div>
                 <div class="input-box re-pass">
                     <input required="" type="password"" class="input-text re-pass">
@@ -69,5 +74,6 @@
         </div>
         <p class="copyright">© 2024 Flouse</p>
     </div>
+    <script src="../JS/Auth/ShowHidePass.js"></script>
 </body>
 </html>
