86e736e1496c4f8be4ea57741c0376347bfa8221
LAPTOP-TTUIQ39L\ichsan <ichsanilyasa3@gmail.com>
2024-05-17 10:03:06 +0700

    update 2.0.7 overlay

--- Flouse/resources/views/Address.blade.php
+++ Flouse/resources/views/Address.blade.php
@@ -35,14 +35,12 @@
                             <div class="location-address-user">
                                 <ploc>Rumah Talenta Bca, Jalan Pakuan No.3, Sumur Batu, Babakan Madang Jawa Barat, Kab. Bogor, Babakan Madang, 16810
                                 </ploc>
-                                <div class="share-change-address">
-                                    <p>Share</p>
-                                    <hr>
+                                <div class="share-change-address" onclick="on()">
                                     <p>Change Address</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="address-box-for-profile-page">
+                        <div class="address-box-for-profile-page coloring">
                             <div class="title-address">
                                 <p2>Home</p2>
                                 <pmain>Main</pmain>
@@ -54,9 +52,7 @@
                             <div class="location-address-user">
                                 <ploc>Rumah Talenta Bca, Jalan Pakuan No.3, Sumur Batu, Babakan Madang Jawa Barat, Kab. Bogor, Babakan Madang, 16810
                                 </ploc>
-                                <div class="share-change-address">
-                                    <p>Share</p>
-                                    <hr>
+                                <div class="share-change-address" onclick="on()">
                                     <p>Change Address</p>
                                 </div>
                             </div>
@@ -65,7 +61,58 @@
                 </div>
             </div>
         </div>
+        <div id="overlay-effect">
+            <div class="overlay-for-address">
+                <div class="title-bar">
+                    <p>Change Address</p>
+                    <div class="off-button" onclick="off()">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z" />
+                        </svg>
+                    </div>
+                </div>
+                <div class="form-for-address">
+                    <div class="per-text-input">
+                        <p>Address Label</p>
+                        <input type="text" placeholder="Label">
+                        <p4>4/20</p4>
+                    </div>
+                    <div class="per-text-input">
+                        <p>Full Address</p>
+                        <textarea name="" id="" cols="5" rows="auto" placeholder="Full Address"></textarea>
+                        <p4>100/200</p4>
+                    </div>
+                    <div class="per-text-input">
+                        <p>Notes for courier</p>
+                        <input type="text" placeholder="Notes">
+                        <p4>10/50</p4>
+                    </div>
+                    <div class="per-text-input">
+                        <p>Receiver' Name</p>
+                        <input type="text" placeholder="Full Name">
+                        <p4>10/50</p4>
+                    </div>
+                    <div class="per-text-input">
+                        <p>Phone Number</p>
+                        <input type="text" placeholder="Phone Number">
+                    </div>
+                </div>
+                <div class="button-for-overlay">
+                    <button>Save</button>
+                </div>
+            </div>
+        </div>
         @include('components/footer')
     </body>
+    <script>
+        function on() {
+            document.getElementById("overlay-effect").style.display = "flex";
+        }
+
+        function off() {
+            document.getElementById("overlay-effect").style.display = "none";
+        }
+    </script>
 
 </html>
