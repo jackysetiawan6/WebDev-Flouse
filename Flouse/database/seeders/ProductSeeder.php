741d3cd563af4435a950c908783734e6375e6c8f
jackysetiawan6 <jackysetiawan6@gmail.com>
2024-06-02 14:52:35 +0000

    Added a sample insertion to Products table.

--- Flouse/database/seeders/ProductSeeder.php
+++ Flouse/database/seeders/ProductSeeder.php
@@ -4,14 +4,18 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('products')->insert([
+            'name' => 'Product 1',
+            'original_price' => 100,
+            'discount' => 10,
+            'final_price' => 90,
+            'description' => 'Product 1 description',
+        ]);
     }
 }
